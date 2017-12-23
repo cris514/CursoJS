942c60743631581570aa1c6ab1e63b6b9b308f0c
cris514 <cristopher.peraza@uabc.edu.mx>
2017-12-22 20:47:34 -0800

    Initial commit

---
+++ Objetos.js
@@ -0,0 +1,18 @@
+//Cap 11.
+//Creacion de un Objeto.
+//Un objeto contiene varios atributos.
+//Se puede crear objetos dentro de objetos, ilimitadamente.
+//La coma en edad despues del 22 permanese, para dar pie a la Creacion del siguiente objeto, dentro del objeto persona.
+var persona = {
+  nombre: "cristopher",
+  apellido: "pe",
+  edad: 22,
+  direccion:{
+    pais: "mexico",
+    ciudad: "ensenada",
+  edificio:{
+    nombre:"Edificio A",
+    telefono:"11212323"
+    }
+  }
+}
