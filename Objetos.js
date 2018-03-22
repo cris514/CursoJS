f968d315ccaa9a35f7088a2649ab6bf8add8fd9b
cris514 <cristopher.peraza@uabc.edu.mx>
2018-03-21 17:43:32 -0700

    Practicas js

--- Objetos.js
+++ Objetos.js
@@ -15,4 +15,27 @@ var persona = {
     telefono:"11212323"
     }
   }
+
 }
+// // Imprimir Objetos.
+// console.log(persona);
+// console.log(persona.nombre);
+// console.log(persona.direccion.ciudad);
+
+
+//Notacion de puntos
+//Esta es la ruta para obtener el valor espesifico, de un objeto dento de otro.
+//Consultar el pais del objeto direccion
+console.log(persona.direccion.pais);
+//Consultar el nombre del objeto edificio
+console.log(persona.direccion.edificio.nombre);
+//Todos los objetos son pasados por referencia.
+//De esta manera podemos asignar un nueva propiedad dentro de un objeto en espesifico.
+var edificio = persona.direccion.edificio;
+edificio.numpiso = "piso 2";
+console.log(persona);
+
+//Notacion de Corchete.
+//Utiliada cuado necesitamos traer datos de manera dinamica.
+ var campo ="edad";
+console.log(persona["campo"]);
